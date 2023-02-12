39c127f5b76e8b91a93f5b7305d47252db974dde
toaayush <toaayush@users.noreply.github.com>
2023-02-12 18:49:01 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-
-import './App.css';
-import Home from './components/Home';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Friends from "./components/Friends";
+import Messages from "./components/Messages";
+import Myprofile from "./components/Myprofile";
+import Explore from "./components/Explore";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} key='home'/>
+          <Route exact path="/friends" element={<Friends />} key='friends'/>
+          <Route exact path="/explore" element={<Explore />} key='explore'/>
+          <Route exact path="/messages" element={<Messages />} key='messages'/>
+          <Route exact path="/myprofile" element={<Myprofile />} key='myprofile'/>
+        </Routes>
+        </BrowserRouter>
     </div>
   );
 }
